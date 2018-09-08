ce14c0424e7ebf2488488018743f9227e4637b04
Chris Jolly <chrisjolly25@gmail.com>
2018-09-08 20:53:18 +1200

    Add redux to react create project. Store consists of a single string: seed. This will be used for randomization, before deterministically selecting cards.
    Use combine reducers to take a function that takes an element one deep from the root of the state, and an action, and returns that element of state updated.
    
    Mapping state to props, to make seed available to app.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import logo from './logo.svg';
 import './App.css';
+
+import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
@@ -8,14 +11,25 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Card Shuffle</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {this.props.seed}
         </p>
       </div>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  seed: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = state => ({
+  seed: state.seed,
+});
+
+export default connect(mapStateToProps,)(App);
--- src/index.js
+++ src/index.js
@@ -4,5 +4,22 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducers from './reducers/reducers';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { storeSeed } from './actions/actions'
+
+// Initialise store. Pass in reducers. Pass in redux devtools extension.
+const store = createStore(
+    reducers, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+store.dispatch(storeSeed('1234'));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root')
+);
 registerServiceWorker();
