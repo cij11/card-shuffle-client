adeb9345a481f7de3a7d4bda8f1b3ce2e4260d38
Chris Jolly <chrisjolly25@gmail.com>
2018-11-03 14:53:39 +1300

    Each deck needs at least as many cards as the number of decks to split into. Aing a few more dummy character cards to character deck. Creating BlankSlate character as template for next characters.

--- src/deck-content/character-deck.js
+++ src/deck-content/character-deck.js
@@ -1,4 +1,4 @@
-import { buildCard, buildDeck } from '../deck-building.js';
+import { buildDeck } from '../deck-building.js';
 
 import character000 from './character-cards/character000.js';
 import character001 from './character-cards/character001.js';
@@ -6,10 +6,39 @@ import character002 from './character-cards/character002.js';
 import character003 from './character-cards/character003.js';
 
 const characterCards = [];
+let character;
+
+character = {
+    data: {
+        title: "BlankSlate",
+        paragraphs: [
+            "CollaborationBetween",
+            "You",
+            "CurrentDM",
+            "AndTheGroup",
+        ],
+        rules: [
+            "YouDecideOne",
+            "CurrentDMDecidesOne",
+            "GroupDecidesOne",
+        ],
+        items: [
+            "YouDecideOne",
+            "CurrentDMDecidesOne",
+            "GroupDecidesOne",
+        ]
+    }
+};
+characterCards.push(character);
+
 
 characterCards.push(character000);
 characterCards.push(character001);
 characterCards.push(character002);
+characterCards.push(character003);
+characterCards.push(character003);
+characterCards.push(character003);
+characterCards.push(character003);
 characterCards.push(character003);
 
 
